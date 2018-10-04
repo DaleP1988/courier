ae329c0a8ff2ce021ebdc1f161fa089a20674e86
KatHe <he.katherine321@gmail.com>
2018-10-04 00:27:22 -0400

    fixed login

--- public/js/html.js
+++ public/js/html.js
@@ -9,6 +9,7 @@ var manualRowCount = 1 // needed for create new manual rows
 $(".logoff").on("click", function() {
     sessionStorage.removeItem('courieruser')
     sessionStorage.removeItem('courierchosen')
+    sessionStorage.removeItem('couriersignin')
     var auth2 = gapi.auth2.getAuthInstance()
     auth2.signOut().then(function () {
       console.log('User signed out.')
